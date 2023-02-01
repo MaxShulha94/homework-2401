ea281cf76c6639700818fcbe5bb94520d7df4c92
MaxShulha94 <maxshulga94@gmail.com>
2023-02-01 17:38:30 +0200

    homework2 01.02

--- module_Group.py
+++ module_Group.py
@@ -6,7 +6,7 @@ class GroupIterator:
         self.index = 0
 
     def __next__(self):
-        if self.index < self.students:
+        if self.index < len(self.students):
             self.index += 1
             return self.students[self.index - 1]
         raise StopIteration
@@ -15,34 +15,41 @@ class Group:
     def __init__(self, specialize, limit_st=10):
         self.specialize = specialize
         self.limit_st = limit_st
-        self.list_group = []
+        self.students = []
 
     def __iter__(self):
-        return GroupIterator(self.list_group)
+        return GroupIterator(self.students)
 
     def __getitem__(self, item):
-        return self.list_group[item]
+        return self.students[item]
 
     def __len__(self):
-        return len(self.list_group)
+        return len(self.students)
 
     def add_st(self, student: module_Student.Student):
-        if len(self.list_group) > self.limit_st:
+        if len(self.students) > self.limit_st:
             raise module_logging.LimitError('You can not add more than 10 students!')
-        if student not in self.list_group:
+        if student not in self.students:
             module_logging.logger.debug(f'Student {student} was added')
-            self.list_group.append(student)
+            self.students.append(student)
 
     def del_st(self, student: module_Student.Student):
-        if student in self.list_group:
-            self.list_group.remove(student)
+        if student in self.students:
+            self.students.remove(student)
 
     def search_st(self, surname: str):
         res = []
-        for i in self.list_group:
+        for i in self.students:
             if i.surname == surname:
                 res.append(i)
         return res
 
     def __str__(self):
-        return f'{self.specialize}:\n' + '\n'.join(map(str, self.list_group))
+        return f'{self.specialize}:\n' + '\n'.join(map(str, self.students))
+
+a = Group('sedf')
+a.students.append('Asd, asdasd')
+a.students.append('dfgtg, dfgsdfgdfg')
+a.students.append('yuid, abgrsd')
+for item in a:
+    print(item)
