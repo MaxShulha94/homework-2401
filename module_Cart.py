0b2aa7d49f3b6f4007e658cf6a7891f8633b5c92
MaxShulha94 <maxshulga94@gmail.com>
2023-02-01 17:41:05 +0200

    homework2 01.02

--- module_Cart.py
+++ module_Cart.py
@@ -1,32 +1,42 @@
 import module_Customer
 import module_Product
 
+class CartIterator:
+    def __init__(self, products):
+        self.products = products
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.products):
+            self.index += 1
+            return self.products[self.index - 1]
+        raise StopIteration
 
 class Cart:
     MAX_LIMIT = 40
 
     def __init__(self, customer: module_Customer.Customer):
         self.customer = customer
-        self.__products = []
+        self.products = []
         self.__quantities = []
 
     def __getitem__(self, item):
-        return self.__products[item]
+        return self.products[item]
 
     def __len__(self):
-        return len(self.__products)
+        return len(self.products)
 
     def get_weight(self):
         return sum(self.__quantities)
 
     def add_product(self, product: module_Product.Product, quantity: int | float = 1):
         if self.get_weight() + quantity <= Cart.MAX_LIMIT:
-            self.__products.append(product)
+            self.products.append(product)
             self.__quantities.append(quantity)
 
     def total(self):
         result = 0
-        for product, quantity in zip(self.__products, self.__quantities):
+        for product, quantity in zip(self.products, self.__quantities):
             result += product.price * quantity
 
         return result
@@ -34,7 +44,7 @@ class Cart:
     def __str__(self):
         result = f'{self.customer}\n'
         result += '\n'.join(map(lambda item: f'{item[0]} x {item[1]} = {item[0].price * item[1]} UAH',
-                                zip(self.__products, self.__quantities)))
+                                zip(self.products, self.__quantities)))
         result += f'\nTotal: {self.total()} UAH'
         return result
 
